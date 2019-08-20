47715c687c13a8d2f1785474463af797441e742a
Stefan Racic <stefracic@gmail.com>
2019-08-20 22:57:36 +0200

    Functional REGISTER USER

---
+++ routes/users.js
@@ -0,0 +1,64 @@
+// Main framework
+const express = require("express");
+// Router
+const router = express.Router();
+// Crypting password
+const bcrypt = require("bcryptjs");
+// JSON Web Token for authorization
+const jwt = require("jsonwebtoken");
+// Config
+const config = require("config");
+// Validation
+const { check, validationResult } = require("express-validator");
+// User Model
+const User = require("../models/User");
+
+// @route POST api/users
+// @desc Register a new user
+// @access Public
+router.post(
+  "/",
+  [
+    check("name", "Please Add Name")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter  a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json.status({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User arleady exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.json({ msg: "User Added" });
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect MongoDB
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ msg: "First GET Method for TaskKeeper API" });
+});
+
+// Defining Routes
+app.use("/api/users", require("./routes/users"));
+// app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/tasks", require("./routes/tasks"));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
